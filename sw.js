6a97500e4e378facc0731974ba2fd7e859297191
Tu4nmud4 <fito96219@gmail.com>
2026-05-20 00:47:33 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='bangun-ruang-3d-v1';
+const ASSETS=['/index.html','/manifest.json'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(cached=>cached||fetch(e.request))
+  );
+});
